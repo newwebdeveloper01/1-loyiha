d9195c4a5137c0cea510f338f0db26e069ee4b4b
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2022-11-13 15:57:27 +0500

    h8urbfb

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-baza.splice(100);
+baza.splice(10);
 
 
 //------------ tartiblanmagan bazani tartiblash---------------//
@@ -21,10 +21,11 @@ const sortBaza = baza.map((baza) => {
 
     }
 })
-console.log(sortBaza)
+// console.log(sortBaza)
 
 function rendorBaza() {
     sortBaza.forEach((el) => {
+
         const card = crElement('div', 'card shadow-lg ', `
         <img src="${el.minIMG}" alt="img" class="card-img">
         <div class="card-body">
@@ -55,7 +56,126 @@ function rendorBaza() {
     
         </div>
         `);
-        $('.blackboard').appendChild(card)
-    })
+
+       $('.salom').appendChild(card)
+   
+   
+      } )
 }
 rendorBaza();
+
+
+
+const sortCotegory=()=>{
+   const arrayCategory=[];
+
+   sortBaza.forEach((e)=>{
+      e.category.forEach((el)=>{
+         if(!arrayCategory.includes(el)){
+            arrayCategory.push(el);
+         }
+
+      })
+   })
+
+   arrayCategory.sort();
+
+   arrayCategory.unshift("All");
+
+   arrayCategory.forEach((el)=>{
+      const option=crElement('option','option-item',el);
+      $('#idCategory').appendChild(option);
+   })
+   console.log(arrayCategory);
+}
+sortCotegory();
+
+
+
+
+
+const findFilm=(regerx,reyting=0,category)=>{
+   if(category=='All'){
+      return sortBaza.filter((films)=>{
+         return films.title.match(regerx) && films.rating>=reyting
+      })
+
+   }
+   return sortBaza.filter((films)=>{
+      return films.title.match(regerx) && films.rating>=reyting && films.category.includes(category)
+   })
+}
+
+$('.formSubmit').addEventListener('submit',  (el)=> {
+   el.preventDefault();
+   const filmName=$('#idName').value;
+   const filmReyting=$('#idReyting').value;
+   const filmCategory=$('#idCategory').value;
+
+   const regerx= new RegExp(filmName,"gi");
+
+   const searchFilm=findFilm(regerx,filmReyting,filmCategory);
+
+   // console.log(searchFilm);
+   setTimeout(()=>{
+      if(searchFilm.length>0){
+         searchResultsRender(searchFilm);
+         $('.info__number').innerHTML=`<strong >${searchFilm.length}</strong> ta ma'lumot topildi`
+
+
+      }
+      else{
+// $('.salom').classList.add('d-none');
+
+$('.salom').innerHTML=`<h1>MA'LUMOT TOPILMADI</h1>`;
+
+$('.info__number').innerHTML=`<strong >${searchFilm.length}</strong> ta ma'lumot topildi`
+      }
+   },200)
+
+
+});
+
+
+
+
+function searchResultsRender(data=[]) {
+   // console.log(data.length);`
+   $('.salom').innerHTML = "";
+   data.forEach((el) => {
+      const card = crElement('div', 'card shadow-lg', 
+      `<img src="${el.minIMG}" alt="img" class="card-img">
+       <div class="card-body">
+          <h4 class="card-title">
+             ${el.title}   
+          </h4>
+          <ul class="list-unstyled">
+             <li> <strong>Year:  ${el.year}   </strong>
+             </li>
+             <li> <strong>Language:  ${el.lang} </strong></li>
+             <li> <strong>Rating:   ${el.rating} </strong></li>
+             <li> <strong>Category:  ${el.category}  </strong></li>
+             <li> <strong>Runtime:  ${el.time} </strong></li>
+          </ul>
+   
+          <div class="social d-flex">
+             <a href="${el.link}" target="_blank" class="btn btn-danger m-2">
+                Trailers
+             </a>
+             <button class="btn btn-primary m-2">
+              Read more . . .
+             </button>
+   
+
+             
+             <button class="btn btn-warning m-2">
+                Add bookmark
+             </button>
+          </div>
+   
+       </div>`);
+
+      $('.salom').appendChild(card);
+
+   })
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -2,7 +2,7 @@ const $=(selector)=> document.querySelector(selector);
 const $$=(selector)=> document.querySelectorAll(selector);
 
 const crElement=function(tagName,className,content){
-    const newElem=document.querySelector(tagName);
+    const newElem=document.createElement(tagName);
     if(className)  newElem.setAttribute('class',className);
     if(content) newElem.innerHTML=content;
     return newElem;
